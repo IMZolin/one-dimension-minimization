44f6566e18c446746abcdb17fc43e43e04759b21
Evgeniy Hlamkin <98908654+GlanceFromTheScreen@users.noreply.github.com>
2023-03-24 22:07:36 +0300

    tpm method + graphics

--- src/One_D_Problem_file.py
+++ src/One_D_Problem_file.py
@@ -1,16 +1,24 @@
+from Trial_Point_Method_file import trial_point_method
 
 
 def target_function(self, x_):
     """целевая функция"""
-    return x_**2
+    return 10 * (((x_ - 1) ** 2) ** (1 / 3)) / (x_ ** 2 + 9)
+
+
+def fun(self, x_):
+    return x_ + 2
 
 
 class One_D_Problem:
     def __init__(self):
-        left_border = 0  # интервал
-        right_border = 1
+        self.left_border = 0.1  # интервал
+        self.right_border = 1.5
+        self.target_function = lambda a: target_function(self, a)
 
-    target_function = target_function
+    # target_function = target_function
+    trial_point_method = trial_point_method
+
     """
     Здесь добавляем методы решения данной задачи
     Методы возвращают два числа: 1) ответ 2) количество обращений к вычислению функции
@@ -24,4 +32,8 @@ class One_D_Problem:
 
 
 p1 = One_D_Problem()
-print(p1.target_function(9))
+print(p1.trial_point_method(0.01))
+p1.target_function = lambda x_: fun(p1, x_)
+print(p1.target_function(8))
+
+
